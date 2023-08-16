3d1a44c50f03ac0f94d83575791773ac9157d143
Pavel <black_yaguar@mail.ru>
2023-08-16 10:50:03 +0500

    Файл для обработки вводимой строки

--- Lesson3/Utility.java
+++ Lesson3/Utility.java
@@ -2,29 +2,32 @@ package lesson3;
 
 import java.util.Scanner;
 
+
 public class Utility{
     public static void start(){
-        String  stroka;
+        String  stroka=" ";
+        String[] data = new String[6];
         info();
         stroka=inputStroka();
         stroka = stroka.replaceAll("\\s+", " ");
         stroka= stroka.trim();
-        String[] data = stroka.split(" ");
-        //System.out.println( Verification.proverka(data));
+        data = stroka.split(" ");
+       
         Verification.proverka(data);
-        //String proverka=Verification.proverka(data);
-        //System.out.println(proverka);
-
+             for (String string : data) {
+            System.out.println(string);
+            
+        }
     }
 
      static void   info(){
         System.out.println( "Введите через пробел свои данные:");
-        System.out.println("\nФорма ввода:\n ФИО - строки\n дата рождения - dd.mm.yyyy\n номер телефона -  целое безнаковое число без форматирования\n пол - символами латиницей f или m ");
+        System.out.println("\nФорма ввода:\n ФИО - строки\n дата рождения - dd.mm.yyyy\n номер телефона -  целое безнаковое число без форматирования\n пол - символами латиницей f или m \n");
     }
     public static  String inputStroka(){
 
         Scanner sc = new Scanner (System.in);
-        System.out.println("\nВведите данные");
+        System.out.println("Введите данные");
         String stroka = sc.nextLine();
        
         return stroka;
